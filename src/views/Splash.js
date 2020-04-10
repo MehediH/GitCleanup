193bc16cfb90fb04f27a623280e14d0125005eb0
MehediH <mehedi.dip@outlook.com>
2020-04-10 14:46:26 +0100

    Update description

--- src/views/Splash.js
+++ src/views/Splash.js
@@ -13,7 +13,7 @@ class Splash extends Component {
                     <img src={lightLogo} alt="GitCleanup logo" draggable="false" className="lightLogo logo"/>
                 </div>
                 <h1>Welcome to GitCleanup</h1>
-                <h2>Clean up your GitHub profile by deleting abandoned or empty repositories with just a few clicks.</h2>
+                <h2>Say hello to GitHub Zero: de-clutter your GitHub profile and get rid of unused repositories with just a few clicks.</h2>
                 <a href="/api/login"><GoMarkGithub/>Login with GitHub</a>
             </div>
         );
